173e18ada8c807de49230ba4f67209a729eb2269
Tsega-Ab Abera <tsega2130@gmail.com>
2022-03-02 10:24:43 -0500

    formatting and fixing app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from jinja2 import Template
 import subprocess #put in args orthovar needs
 
 app = Flask(__name__) 
@@ -9,8 +10,10 @@ app = Flask(__name__)
 #UPLOAD_FOLDER = 'Users\tsabera\Desktop\OrthoVar_Flask\UPLOAD_FOLDER'
 #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#size restriction to prevent
+#size restriction to prevent overloading
 app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024
+
+#check what type file is coming in as before passing into subprocess 
 app.config["UPLOAD_EXTENSIONS"] = ['.txt', '.fasta']
 
 
@@ -37,39 +40,30 @@ def upload_file():
     hum_access = ''
     hum_access = human_file_string[0].split(" ")[0][1:]
     print(hum_access)
-    
+
+    #Why is the passed variable uploaded_file.filename?
     if uploaded_file1.filename != '':
         uploaded_file1.save(uploaded_file1.filename)
     if uploaded_file2.filename != "":
         uploaded_file2.save(uploaded_file2.filename)
     
     
-    #Is the issue test_orthovar or subprocess?
-    #Why is the passed variable uploaded_file.filename?
-    
     #print(subprocess.run(f"python test_OrthoVar.py -O {uploaded_file2.filename} -H {uploaded_file1.filename}", shell=True, capture_output=True))
     #print(subprocess.CompletedProcess(args=[f"python OrthoVar.py -O {uploaded_file2.filename} -H {uploaded_file1.filename}"]))
-
-    #result = subprocess.run(["test_OrthoVar.py", "-c" "-O {uploaded_file2.filename} -H {uploaded_file1.filename}"])
-    #print(result)
-    
-    #subprocess.run(["test_OrthoVar.py", "-O {uploaded_file2.filename} -H {uploaded_file1.filename}"  ])
-    #print(subprocess.run(["cat", "data.txt"])) 
-    
-    #result = subprocess.run(["cat", "data.txt"], capture_output = True)
-    #print(result)
     
     #text captures it as a string 
-    result = subprocess.run(f"python test_OrthoVar.py -O {uploaded_file2.filename} -H {uploaded_file1.filename}", shell = True, text = True, capture_output = True)
+    #using shell = true has shell injection security implications?
+
+    #HERE!! Most recent test 
+    result = subprocess.run(f"python OrthoVar.py -O {uploaded_file2.filename} -H {uploaded_file1.filename}", shell = True, text = True, capture_output = True)
     print(result)
     print("std.out:", result.stdout)
    
-    #using shell =true has shell injection security implications
-    output = subprocess.run("cat 'data.txt'", shell = True, text = True, capture_output = True)
-    print(output)
-    print(output.stdout)
+    #Testing to see if the issue is test_orthovar or subprocess
+    wowza = subprocess.run("cat 'data.txt'", shell = True, text = True, capture_output = True)
+    print(wowza.stdout)
+    print("here")
     
-    return render_template("uploader.html")
+    return render_template("uploader.html", output = wowza.stdout, download_file = wowza.stdout)
 
 
-#check what type file is coming in as before passing into subprocess 
