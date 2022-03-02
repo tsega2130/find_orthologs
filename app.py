d856ef4739e0fcf95689554a69b759b9f8732fd9
possiblyMikeB <miblackmon@davidson.edu>
2022-03-02 10:27:21 -0500

    some more fixes to POST route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import os
+import os, sys
+from tempfile import NamedTemporaryFile
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
-from jinja2 import Template
 import subprocess #put in args orthovar needs
 
 app = Flask(__name__) 
@@ -10,10 +10,8 @@ app = Flask(__name__)
 #UPLOAD_FOLDER = 'Users\tsabera\Desktop\OrthoVar_Flask\UPLOAD_FOLDER'
 #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#size restriction to prevent overloading
+#size restriction to prevent
 app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024
-
-#check what type file is coming in as before passing into subprocess 
 app.config["UPLOAD_EXTENSIONS"] = ['.txt', '.fasta']
 
 
@@ -27,43 +25,29 @@ def upload_file():
     #gets file from multi-key dictionary 
     uploaded_file1 = request.files['Human_Fasta_File']
     uploaded_file2 = request.files['Orthologs_Fasta_File']
-
-    human_file_string = str(uploaded_file1.read().decode("utf-8"))
-    ortho_file_string = str(uploaded_file2.read().decode("utf-8"))
-
-    human_file_string = human_file_string.split("\n")
-    ortho_file_string = ortho_file_string.split("\n")
-    #print(human_file_string)
-    #print(ortho_file_string)
-    print(type(ortho_file_string))
-
-    hum_access = ''
-    hum_access = human_file_string[0].split(" ")[0][1:]
-    print(hum_access)
-
-    #Why is the passed variable uploaded_file.filename?
-    if uploaded_file1.filename != '':
-        uploaded_file1.save(uploaded_file1.filename)
-    if uploaded_file2.filename != "":
-        uploaded_file2.save(uploaded_file2.filename)
     
-    
-    #print(subprocess.run(f"python test_OrthoVar.py -O {uploaded_file2.filename} -H {uploaded_file1.filename}", shell=True, capture_output=True))
-    #print(subprocess.CompletedProcess(args=[f"python OrthoVar.py -O {uploaded_file2.filename} -H {uploaded_file1.filename}"]))
-    
-    #text captures it as a string 
-    #using shell = true has shell injection security implications?
+    # generate temporary files 
+    human_file = NamedTemporaryFile('w')
+    ortho_file = NamedTemporaryFile('w')
 
-    #HERE!! Most recent test 
-    result = subprocess.run(f"python OrthoVar.py -O {uploaded_file2.filename} -H {uploaded_file1.filename}", shell = True, text = True, capture_output = True)
-    print(result)
-    print("std.out:", result.stdout)
-   
-    #Testing to see if the issue is test_orthovar or subprocess
-    wowza = subprocess.run("cat 'data.txt'", shell = True, text = True, capture_output = True)
-    print(wowza.stdout)
-    print("here")
+    # save uploads to temparay file locations
+    uploaded_file1.save(human_file.name)
+    uploaded_file2.save(ortho_file.name)
     
-    return render_template("uploader.html", output = wowza.stdout, download_file = wowza.stdout)
+    # run orthovar with temp file input
+    result=subprocess.run(
+        [sys.executable, "OrthoVar.py", "-O", ortho_file.name, "-H", human_file.name],
+        capture_output=True, 
+        text=True
+    )
+    
+    # check for errors
+    if result.returncode > 0:
+        print(result.stderr)
+        raise Exception("Error non-zero return code from OrthoVar")
+    
+    # render template with output
+    return render_template("uploader.html", output=result.stdout)
 
 
+#check what type file is coming in as before passing into subprocess 
